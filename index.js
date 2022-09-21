943d8efdb97c2c8a6cc2524e10cf8248138ed6d4
Sophie Aulitzky <sophie.aulitzky@gmail.com>
2022-09-21 11:02:49 +0200

    Update changes post review

--- index.js
+++ index.js
@@ -7,20 +7,14 @@ const url = 'https://memegen-link-examples-upleveled.netlify.app/';
 const response = await fetch(url);
 const body = await response.text();
 
-const y = load(body);
+const data = load(body);
 
 for (let count = 0; count < 10; count++) {
-  // imagesUrls.push(y('div > a > img')[i].attribs.src);
-  const storeURls = y('div > a > img')[count].attribs.src;
-  console.log(storeURls);
+  const storeURls = data('div > a > img')[count].attribs.src;
   const response2 = await fetch(storeURls);
   const imageData = await response2.arrayBuffer();
   console.log(imageData);
   const buffer = Buffer.from(imageData);
   fs.mkdir('memes', () => {});
-  fs.writeFile(`memes/meme0${count + 1}.jpg`, buffer, () => {});
+  fs.writeFile(`memes/0${count + 1}.jpg`, buffer, () => {});
 }
-
-// console.log(Object.keys(imagesUrls[1]));
-// console.log(imagesUrls);
-// left to do: make sure that the files start at 1 and end at 10; create one single folder for all files
